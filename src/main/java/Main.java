326329a4280f8217063e4869d5fd52be614c55df
Dagmara <dagmarakitkowska@gmail.com>
2020-03-01 12:25:00 +0100

    drugi komentarz

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * ...comment class...
+ *
+ * @author dagmarakitkowska@gmail.com
+ * @since 01.03.2020
+ */
+final class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
